6b2a3f6ea98c7ae1d94b4e28bf97c763e0bc9046
Alma Jasper <almajasper04@gmail.com>
2024-05-26 13:28:23 +0100

    code to extract list of values from coord pairs

--- terrain_analysis.py
+++ terrain_analysis.py
@@ -19,11 +19,21 @@ def convert_to_rasterio(raster_data, template_raster):
 
 
 def extract_values_from_raster(raster, shape_object):
-    #raster.transform(shape_object)
-    print(shape_object[0])
-    print(shape_object[1])
+    ''' https://gis.stackexchange.com/questions/317391/extracting-raster-values-at-point-locations-using-python
+    '''
+    ''' https://geopandas.org/en/stable/gallery/geopandas_rasterio_sample.html
+        https://blog.hubspot.com/website/python-zip#:~:text=The%20%60zip%60%20function%20in%20Python,from%20all%20the%20input%20iterables.
+    ''' 
+    gt = raster.transform
+
+    #coord_list = [(x, y) for x, y in zip(shape_object["geometry"].x, shape_object["geometry"].y)]
+    #coord_list = [(340467.5710219807, 3843321.793904966)]
+    #print(coord_list)
+    vls = raster.sample((340467.5710219807, 3843321.793904966)) 
+    for current_vl in vls:
+        print(current_vl)
     
-    return ([0,1])
+    return vls
 
 
 def make_classifier(x, y, verbose=False):
@@ -47,7 +57,8 @@ def create_dataframe(topo, geo, lc, dist_fault, slope, shape, landslides):
     ''' https://geopandas.org/en/stable/docs/reference/api/geopandas.GeoDataFrame.html
         flatten to change from multimentional ND to 1d
     '''
-    #d = {['elev','fault', 'slope', 'LC','Geol', 'ls']}
+    print(shape)
+
     print("ENTRY create_dataframe")
     d = {'elev':topo.read(1).flatten(),
          'fault':geo.read(1).flatten(),
@@ -56,7 +67,7 @@ def create_dataframe(topo, geo, lc, dist_fault, slope, shape, landslides):
          'Geol':geo.read(1).flatten(),
          'ls':landslides}
  
-    #print(d)
+    print(d)
     df = pd.DataFrame(d)
     print(df)
     gf = gpd.geodataframe.GeoDataFrame(df)
